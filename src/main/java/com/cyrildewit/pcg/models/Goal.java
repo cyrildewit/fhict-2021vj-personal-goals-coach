a72312a417e5a44f6157104fa39c44242765b98b
Cyril de Wit <cyrilwit@gmail.com>
2021-03-29 13:36:21 +0200

    feat: create Goal data model

---
+++ src/main/java/com/cyrildewit/pcg/models/Goal.java
@@ -0,0 +1,68 @@
+package com.cyrildewit.pcg.models;
+
+import java.time.LocalDateTime;
+
+public class Goal {
+    protected int id;
+    protected String uuid;
+    protected String title;
+    protected String description;
+    protected LocalDateTime deadline;
+
+    public Goal() {}
+
+    public Goal(String uuid, String title, String description, LocalDateTime deadline) {
+        this.uuid = uuid;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+    }
+
+    public Goal(int id, String uuid, String title, String description, LocalDateTime deadline) {
+        this.id = id;
+        this.uuid = uuid;
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+}
