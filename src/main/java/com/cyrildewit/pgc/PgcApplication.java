e762d77221eb52d653a460fd7b990a14e6fc21ef
Cyril de Wit <cyrilwit@gmail.com>
2021-04-19 17:49:44 +0200

    feat: setup Spring boot

---
+++ src/main/java/com/cyrildewit/pgc/PgcApplication.java
@@ -0,0 +1,13 @@
+package com.cyrildewit.pgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyrildewit/pgc/PgcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyrildewit.pgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
