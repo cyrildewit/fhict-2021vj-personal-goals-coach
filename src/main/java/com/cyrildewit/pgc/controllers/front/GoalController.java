f2dc6f6f82bf0e10ebe24ab7bb33eac52880cfd7
Cyril de Wit <cyrilwit@gmail.com>
2021-04-21 23:17:14 +0200

    feat: pass goals to view

--- src/main/java/com/cyrildewit/pgc/controllers/front/GoalController.java
+++ src/main/java/com/cyrildewit/pgc/controllers/front/GoalController.java
@@ -1,17 +1,42 @@
 package com.cyrildewit.pgc.controllers.front;
 
+import java.util.ArrayList;
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cyrildewit.pgc.models.Goal;
+import com.cyrildewit.pgc.services.GoalService;
+
 @Controller
 @RequestMapping("/goals")
 public class GoalController {
+    private final GoalService goalService;
+
+    @Autowired
+    public GoalController(GoalService goalService) {
+        this.goalService = goalService;
+    }
 
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        ArrayList<Goal> goals = new ArrayList<Goal>();
+        goals.add(new Goal(
+                2,
+                UUID.randomUUID(),
+                "Six Summit",
+                "Lorem ipsum",
+                LocalDateTime.now()
+        ));
+
+        model.addAttribute("goals", goals);
+
         return "front/goal/index";
     }
 
