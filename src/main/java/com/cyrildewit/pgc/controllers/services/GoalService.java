25dc2b6d03f8a077823994657641aba3c8968896
Cyril de Wit <cyrilwit@gmail.com>
2021-04-21 23:17:26 +0200

    feat: create GoalService

---
+++ src/main/java/com/cyrildewit/pgc/controllers/services/GoalService.java
@@ -0,0 +1,16 @@
+package com.cyrildewit.pgc.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoalService {
+//    public ArrayList<Goal> getAllGoals() {
+//        new Goal(
+//      c          2,
+//                UUID.randomUUID(),
+//                "Six Summit",
+//                "Lorem ipsum",
+//                LocalDateTime.now()
+//        )
+//    }
+}
