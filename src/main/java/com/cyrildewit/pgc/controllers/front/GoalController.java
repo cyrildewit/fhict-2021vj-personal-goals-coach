c6d007fa9ca3baa219f7259a12c13d378ff0824e
Cyril de Wit <cyrilwit@gmail.com>
2021-04-22 09:40:13 +0200

    feat: add create and show controller action

--- src/main/java/com/cyrildewit/pgc/controllers/front/GoalController.java
+++ src/main/java/com/cyrildewit/pgc/controllers/front/GoalController.java
@@ -2,6 +2,7 @@ package com.cyrildewit.pgc.controllers.front;
 
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.Optional;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cyrildewit.pgc.models.Goal;
 import com.cyrildewit.pgc.services.GoalService;
+import com.cyrildewit.pgc.exceptions.GoalNotFoundException;
 
 @Controller
 @RequestMapping("/goals")
@@ -31,4 +34,19 @@ public class GoalController {
         return "front/goals/index";
     }
 
+    @GetMapping("/create/{uuid}")
+    public String create() {
+        return "front/goals/create";
+    }
+
+    @GetMapping("/{uuid}")
+    public String show(@PathVariable("uuid") UUID uuid, Model model) {
+        Optional<Goal> goal = goalService.findGoalByUuid(uuid);
+
+        goal.orElseThrow(() -> new GoalNotFoundException(uuid));
+
+        model.addAttribute("goal", goal.get());
+
+        return "front/goals/show";
+    }
 }
