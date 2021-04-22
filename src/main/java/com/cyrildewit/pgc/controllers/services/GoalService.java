bdb5c2116c4347a7c73bd4829fba97b732a965f9
Cyril de Wit <cyrilwit@gmail.com>
2021-04-22 09:25:34 +0200

    feat: implement GoalService

--- src/main/java/com/cyrildewit/pgc/controllers/services/GoalService.java
+++ src/main/java/com/cyrildewit/pgc/controllers/services/GoalService.java
@@ -1,16 +1,66 @@
 package com.cyrildewit.pgc.services;
 
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cyrildewit.pgc.models.Goal;
+import com.cyrildewit.pgc.models.User;
+import com.cyrildewit.pgc.daos.GoalDao;
+import com.cyrildewit.pgc.daos.InMemoryGoalDao;
 
 @Service
-public class GoalService {
-//    public ArrayList<Goal> getAllGoals() {
-//        new Goal(
-//      c          2,
-//                UUID.randomUUID(),
-//                "Six Summit",
-//                "Lorem ipsum",
-//                LocalDateTime.now()
-//        )
-//    }
+public class GoalService
+{
+    private final GoalDao goalDao;
+
+    @Autowired
+    public GoalService(InMemoryGoalDao goalDao)
+    {
+        this.goalDao = goalDao;
+    }
+
+    public List<Goal> getAllGoals()
+    {
+        return goalDao.selectAllGoals();
+    }
+
+    public List<Goal> getAllGoalsForUser(User user)
+    {
+        return goalDao.selectAllGoalsForUser(user);
+    }
+
+    public Optional<Goal> findGoalById(Integer id)
+    {
+        return goalDao.findGoalById(id);
+    }
+
+    public Optional<Goal> findGoalByUuid(UUID uuid)
+    {
+        return goalDao.findGoalByUuid(uuid);
+    }
+
+    public void addGoal(Goal goal)
+    {
+        goalDao.insertGoal(goal);
+    }
+
+    public void updateGoal(Goal goal, String[] params)
+    {
+        goalDao.insertGoal(goal);
+    }
+
+    public void deleteGoalById(Integer id)
+    {
+        goalDao.deleteGoalById(id);
+    }
+
+    public void deleteGoal(Goal goal)
+    {
+        goalDao.deleteGoal(goal);
+    }
 }
