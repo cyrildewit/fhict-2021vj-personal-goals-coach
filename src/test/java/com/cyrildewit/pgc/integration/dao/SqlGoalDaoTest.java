5815a321ceef6bd40987c07ea3506ed082fd8001
Cyril de Wit <cyrilwit@gmail.com>
2021-05-27 17:51:55 +0200

    test: SqlGoalDao.deleteGoal()

--- src/test/java/com/cyrildewit/pgc/integration/dao/SqlGoalDaoTest.java
+++ src/test/java/com/cyrildewit/pgc/integration/dao/SqlGoalDaoTest.java
@@ -42,10 +42,6 @@ class SqlGoalDaoTest {
 
     @Test
     void selectAllGoals() {
-        // Database seeding
-//        final String SQL_STATEMENTS = "TRUNCATE TABLE goals;" +
-//                "INSERT INTO goals (uuid, title, description, deadline, user_id, created_at, updated_at) VALUES ('16bc6513-18df-4530-ab20-cb5e76412544', 'Test Goal 1', 'Goal description', '2021-05-10 20:15:25', 1, '2021-05-10 20:15:25', '2021-05-10 20:15:25');" +
-//                "INSERT INTO goals (uuid, title, description, deadline, user_id, created_at, updated_at) VALUES ('c38aeed1-9077-4194-9d6c-fe0fc3f12345', 'Test Goal 2', 'Goal description', '2021-05-10 20:15:25', 1, '2021-05-10 20:15:25', '2021-05-10 20:15:25');";
         try (Connection connection = mariaDBDriver.getConnection();
              Statement stmt = connection.createStatement();) {
 
@@ -265,6 +261,62 @@ class SqlGoalDaoTest {
         }
 
         goalDao.deleteGoalById(goal.getId());
+
+        String GET_GOALS_COUNT = "SELECT count(*) AS goals_count FROM goals;";
+        long goalsCount = -1;
+
+        try (Connection connection = mariaDBDriver.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_GOALS_COUNT);) {
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                goalsCount = result.getLong("goals_count");
+            }
+        } catch (SQLException e) {
+            mariaDBDriver.printSQLException(e);
+        }
+
+        assertEquals(0, goalsCount);
+    }
+
+    @Test
+    void deleteGoal() {
+        Goal goal = new Goal(UUID.randomUUID(), "Goal Title 1", "Description here.", LocalDateTime.now(), 1L);
+        String INSERT_GOAL_SQL = "INSERT INTO goals (uuid, title, description, deadline, user_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+        try (Connection connection = mariaDBDriver.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_GOAL_SQL);
+             Statement stmt = connection.createStatement();) {
+            stmt.execute("TRUNCATE TABLE goals;");
+
+            preparedStatement.setString(1, goal.getUuid().toString());
+            preparedStatement.setString(2, goal.getTitle());
+            preparedStatement.setString(3, goal.getDescription());
+            preparedStatement.setString(4, goal.getDeadline().format(dateTimeFormatters.getMariaDbDateTimeFormatter()));
+            preparedStatement.setLong(5, goal.getUserId());
+            preparedStatement.setString(6, LocalDateTime.now().format(dateTimeFormatters.getMariaDbDateTimeFormatter()));
+            preparedStatement.setString(7, LocalDateTime.now().format(dateTimeFormatters.getMariaDbDateTimeFormatter()));
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            mariaDBDriver.printSQLException(e);
+        }
+
+        final String GET_GOAL_ID_SQL = "SELECT id FROM goals WHERE uuid = ?;";
+
+        try (Connection connection = mariaDBDriver.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_GOAL_ID_SQL);) {
+            preparedStatement.setString(1, goal.getUuid().toString());
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                goal.setId(result.getLong("id"));
+            }
+        } catch (SQLException e) {
+            mariaDBDriver.printSQLException(e);
+        }
+
+        goalDao.deleteGoal(goal);
 
         String GET_GOALS_COUNT = "SELECT count(*) AS goals_count FROM goals;";
         long goalsCount = -1;
