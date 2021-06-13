63b21f3aee54b1588a88d646610514da7ddd7718
Cyril de Wit <cyrilwit@gmail.com>
2021-06-13 12:38:32 +0200

    feat: add response status to NotAuthenticatedException

--- src/main/java/com/cyrildewit/pgc/application/exceptions/NotAuthenticatedException.java
+++ src/main/java/com/cyrildewit/pgc/application/exceptions/NotAuthenticatedException.java
@@ -1,5 +1,9 @@
 package com.cyrildewit.pgc.application.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class NotAuthenticatedException extends RuntimeException
 {
     public NotAuthenticatedException()
